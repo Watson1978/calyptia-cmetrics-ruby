36294bd010d3b682a49e9364edc5ecec817a94a2
Shizuo Fujita <fujita@clear-code.com>
2025-01-25 15:32:30 +0900

    no-error

--- ext/cmetrics/extconf.rb
+++ ext/cmetrics/extconf.rb
@@ -46,7 +46,7 @@ class BuildCMetrics
     @recipe = MiniPortileCMake.new("cmetrics", @version, **kwargs)
     def @recipe.cmake_compile_flags
       flags = super
-      flags << "-DCMAKE_C_FLAGS='-Wno-incompatible-pointer-types -Wno-format -Wno-unused-but-set-variable'"
+      flags << "-DCMAKE_C_FLAGS='-Wno-incompatible-pointer-types -Wno-format -Wno-error'"
     end
     @checkpoint = ".#{@recipe.name}-#{@recipe.version}.installed"
     @recipe.target = File.join(ROOT, "ports")
