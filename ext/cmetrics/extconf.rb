2dd1d45eb3b0dfdaf55e26ba3059270457b13b43
Shizuo Fujita <fujita@clear-code.com>
2025-01-26 14:41:49 +0900

    CMAKE_OBJECT_PATH_MAX

--- ext/cmetrics/extconf.rb
+++ ext/cmetrics/extconf.rb
@@ -47,6 +47,7 @@ class BuildCMetrics
     def @recipe.cmake_compile_flags
       flags = super
       flags << "-DCMAKE_C_FLAGS='-Wno-incompatible-pointer-types -Wno-format -Wno-unused-but-set-variable'"
+      flags << "-DCMAKE_OBJECT_PATH_MAX=1024"
     end
     @checkpoint = ".#{@recipe.name}-#{@recipe.version}.installed"
     @recipe.target = File.join(ROOT, "ports")
